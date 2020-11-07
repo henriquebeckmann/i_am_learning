df7e7a2eb0289ac7b06749aec61f1a01efff7b6c
Henrique Beckmann <henriquebeckmann2@gmail.com>
2020-11-07 13:54:47 -0300

    modified secret key

--- Python/first_django_app/mysite/mysite/settings.py
+++ Python/first_django_app/mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nome372)xrp%675p3y6^%3j4_=zj#p7u!4nu+6q-!ri!zt)94o'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
