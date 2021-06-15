bab1461eeaa2bb5502dd015a950412c9c1a89242
Henrique Beckmann <henriquebeckmann2@gmail.com>
2021-06-15 14:04:40 -0300

    concise control flow with if let

---
+++ Rust/concise_control_flow_with_if_let/src/main.rs
@@ -0,0 +1,31 @@
+#![allow(unused)]
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let some_u8_value = Some(0u8);
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let coin = Coin::Penny;
+
+    let mut count = 0;
+
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
